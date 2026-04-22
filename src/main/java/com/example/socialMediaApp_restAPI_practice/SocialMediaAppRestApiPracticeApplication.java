bb12bd102b506a0a402312079f1c6cc940d2e10b
DeepaksinghDD <deepaksds65@gmail.com>
2026-04-22 17:54:53 +0530

    Initial Commit: Social Media RestiAPI

---
+++ src/main/java/com/example/socialMediaApp_restAPI_practice/SocialMediaAppRestApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialMediaApp_restAPI_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAppRestApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaAppRestApiPracticeApplication.class, args);
+	}
+
+}
